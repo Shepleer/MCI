b3383f2635332866353c9c5e94e7ecdd986be47e
andrew-andilevko-itechart <andrei.andzileuka@itechart-group.com>
2019-12-08 15:09:56 +0300

    Add contact form

--- src/components/reusable/forms/inputs/commentInput/CommentInput.js
+++ src/components/reusable/forms/inputs/commentInput/CommentInput.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const CommentsInput = () => {
   return (
-    // <fieldset className="input-fieldset">
-    //   <legend className="input-legend">Сообщение</legend>
+    <fieldset className="input-fieldset">
+      <legend className="input-legend">Сообщение</legend>
       <textarea className="multiline-input" name="message" cols="1" rows="1" placeholder="Оставьте сообщение" />
-    // </fieldset>
+    </fieldset>
   );
 };
 
