16c2367e81d319106296623e282abf96b4e2045a
Shepleer <true.man.mow@gmail.com>
2019-12-14 23:07:53 +0300

    CMS

---
+++ src/pages/lmia.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import DescriptionWrapper from '../components/DescriptionWrapper/DescriptionWrapper';
+import Layout from '../components/layout';
+
+const LMIA = () => {
+  return (
+    <Layout>
+      <DescriptionWrapper title="Оценка Рынка Труда или LMIA">
+
+      </DescriptionWrapper>
+    </Layout>
+  );
+};
