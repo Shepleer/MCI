2c653485f408568259079ebe785fed2a0f44f11f
ivan <true.man.mow@gmail.com>
2020-01-05 21:04:24 +0300

    form

--- src/components/reusable/forms/feedbackCall/FeedbackCall.js
+++ src/components/reusable/forms/feedbackCall/FeedbackCall.js
@@ -23,11 +23,15 @@ const FeedbackCall = () => {
     <div className="feedback-call-container">
       <h4>Для более подробной информации об услуге оставьте свои данные</h4>
       <p className="description-label">Наш специалист свяжется с вами в течение 30 минут</p>
-      <form name="Phone contact" netlify netlify-honeypot="bot-field" hidden>
-        <input type="tel" name="phone" />
-      </form>
-      <form name="phone" onSubmit={handleSubmit} className="feedback-call-form">
-        <PhoneInput value={phone} onChange={setPhone} country={"ru"} regions={'europe'}  />
+      <form name="Contact phone"
+            onSubmit={handleSubmit}
+            className="feedback-call-form"
+            method="post"
+            data-netlify="true"
+            data-netlify-honeypot="bot-field"
+            onSubmit={handleSubmit}>
+        <input type="hidden" name="form-name" value="Contact phone" />
+        <PhoneInput value={phone} onChange={setPhone} country={"ru"} regions={'europe'} name="phone" />
         <SubmitButton title="ОСТАВИТЬ ЗАЯВКУ" type="submit" shadow />
       </form>
       <p className="policy-label" >Нажимая на кнопку, вы даете согласие на обработку персональных данных и соглашаетесь c политикой
