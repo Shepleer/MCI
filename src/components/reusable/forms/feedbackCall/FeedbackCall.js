b231c3b939d021c12a715f3e16a85055e2164d6a
ivan <true.man.mow@gmail.com>
2020-01-05 21:28:12 +0300

    form

--- src/components/reusable/forms/feedbackCall/FeedbackCall.js
+++ src/components/reusable/forms/feedbackCall/FeedbackCall.js
@@ -10,44 +10,31 @@ const FeedbackCall = () => {
   const [phone, setPhone] = useState('+');
 
   const handleSubmit = useCallback((e) => {
-    fetch("/", {
+    fetch("https://epic-shockley-4c3cca.netlify.com", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({ "form-name": "Phone contact", "phone": phone })
+      body: encode({ "form-name": "Contact phone", "phone": phone })
     });
     e.preventDefault();
   });
 
   return (
     <div className="feedback-call-container">
-      {/*<h4>Для более подробной информации об услуге оставьте свои данные</h4>*/}
-      {/*<p className="description-label">Наш специалист свяжется с вами в течение 30 минут</p>*/}
-      {/*<form name="Contact phone"*/}
-      {/*      onSubmit={handleSubmit}*/}
-      {/*      className="feedback-call-form"*/}
-      {/*      method="post"*/}
-      {/*      data-netlify="true"*/}
-      {/*      data-netlify-honeypot="bot-field"*/}
-      {/*      onSubmit={handleSubmit}>*/}
-      {/*  <input type="hidden" name="form-name" value="Contact phone" />*/}
-      {/*  <PhoneInput value={phone} onChange={setPhone} country={"ru"} regions={'europe'} />*/}
-      {/*  /!*<SubmitButton title="ОСТАВИТЬ ЗАЯВКУ" type="submit" shadow />*!/*/}
-      {/*  <button type="submit" title="submit" ></button>*/}
-      {/*</form>*/}
-      {/*<p className="policy-label" >Нажимая на кнопку, вы даете согласие на обработку персональных данных и соглашаетесь c политикой*/}
-      {/*  конфиденциальности.</p>*/}
-
-      <form onSubmit={handleSubmit}>
-        <p>
-          <label>
-            Your Name: <input type="text" name="phone" value={phone} onChange={e => setPhone(e.target.value)} />
-          </label>
-        </p>
-        <p>
-          <button type="submit">Send</button>
-        </p>
+      <h4>Для более подробной информации об услуге оставьте свои данные</h4>
+      <p className="description-label">Наш специалист свяжется с вами в течение 30 минут</p>
+      <form name="Contact phone"
+            onSubmit={handleSubmit}
+            className="feedback-call-form"
+            method="post"
+            data-netlify="true"
+            data-netlify-honeypot="bot-field"
+            onSubmit={handleSubmit}>
+        <input type="hidden" name="form-name" value="Contact phone" />
+        <PhoneInput value={phone} onChange={setPhone} country={"ru"} regions={'europe'} />
+        <SubmitButton title="ОСТАВИТЬ ЗАЯВКУ" type="submit" shadow />
       </form>
-
+      <p className="policy-label" >Нажимая на кнопку, вы даете согласие на обработку персональных данных и соглашаетесь c политикой
+        конфиденциальности.</p>
     </div>
   );
 };
